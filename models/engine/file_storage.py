ad29e4eed080de01d57ad4ad2c788ff07300a460
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-04-05 14:31:35 +0300

    Add features 'Delete object' and 'Select all type(Object)'

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls:
+            grp = {}
+            for key, val in FileStorage.__objects.items():
+                if val.to_dict()['__class__'] ==\
+                   str(cls).split('.')[-1].split("'")[0]:
+                    grp.update({key: val})
+            return grp
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +53,17 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ delete given object """
+        if obj:
+            x = obj.to_dict()['__class__'] + '.' + obj.to_dict()['id']
+            print(x)
+        else:
+            return
+
+        del(self.all()[x])
+        self.save()
