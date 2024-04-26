1ad49b521bc533d691663fb14c06547bc3a0358a
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-04-26 16:55:21 +0300

    Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,26 +25,20 @@ def do_pack():
 
 def do_deploy(archive_path):
     """ deploy tarball """
-    if os.path.exists(f"{archive_path}"):
-        try:
-            archive = archive_path.split("/")[1]
-            Uncompressed_file = archive_path.split("/")[1].split(".")[0]
-            put(archive_path, "/tmp/")
-            run(f"mkdir -p /data/web_static/releases/{Uncompressed_file}/")
-            run(f"tar -xzf /tmp/{archive} \
--C /data/web_static/releases/{Uncompressed_file}/")
-            run(f"rm /tmp/{archive}")
-            run(f"mv -f /data/web_static/releases/\
-{Uncompressed_file}/web_static/*\
- /data/web_static/releases/{Uncompressed_file}/")
-            run(f"rm -rf /data/web_static/releases/\
-{Uncompressed_file}/web_static")
-            run(f"rm -rf /data/web_static/current")
-            run(f"ln -sf /data/web_static/releases/\
-{Uncompressed_file}/ /data/web_static/current")
-            print("New version deployed!")
-            return True
-        except Exception as e:
-            return False
+    if os.path.exists(archive_path):
+        archive = archive_path.split("/")[1]
+        Uncompressed_file = archive_path.split("/")[1].split(".")[0]
+        put(archive_path, "/tmp/")
+        run(f"mkdir -p /data/web_static/releases/{Uncompressed_file}/")
+        run(f"tar -xzf /tmp/{archive} -C /data/web_static/releases/{Uncompressed_file}/")
+        run(f"rm /tmp/{archive}")
+        run(f"mv -f /data/web_static/releases/{Uncompressed_file}/web_static/* /data/web_static/releases/{Uncompressed_file}/")
+        run(f"rm -rf /data/web_static/releases/{Uncompressed_file}/web_static")
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -sf /data/web_static/releases/{Uncompressed_file}/ /data/web_static/current")
+        print("New version deployed!")
+        return True
     else:
+        print("Error")
         return False
+    return False
