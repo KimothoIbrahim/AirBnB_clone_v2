772c17da4b9f5c01f428a88115f34a0577face39
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-04-27 21:12:37 +0300

    Puppet provision

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,37 @@
+package { 'nginx':
+  ensure => 'installed'
+}
+
+file { '/data':
+  ensure => 'directory',
+  owner  => 'kimotho',
+  group  => 'kimotho'
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => 'This is my Website',
+  require => File['/data']
+}
+
+file { '/data/web_static/shared/':
+  ensure  => 'directory',
+  require => File['/data']
+}
+
+file { '/data/web_static/current/':
+  target  => '/data/web_static/releases/test/',
+  require => File['/data']
+}
+
+exec { 'update_config':
+  command => '/usr/bin/sed -i /^"\tserver_name _"/a\ "\\ \n\tlocation\
+ /hbnb_static{\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default',
+  notify  => Service['nginx'],
+  require => Package['nginx']
+}
+
+service { 'nginx':
+  ensure => 'running',
+  enable => true
+}
