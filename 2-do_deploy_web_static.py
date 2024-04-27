76b4e413d2058eff7b39382ccf0fbb7dde7fd46b
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-04-27 13:41:08 +0300

    Do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,39 +6,31 @@ import datetime
 import os
 
 env.hosts = ['100.25.158.57', '34.203.75.71']
-
-
-def do_pack():
-    """ tar """
-    date = datetime.datetime.now()
-    st = f"{date.year}{date.month}{date.day}\
-{date.hour}{date.minute}{date.second}"
-
-    try:
-        local("if [ ! -d versions ]; then mkdir versions; fi")
-        local(f"tar -czvf versions/web_static_{st}.tgz  web_static/")
-        print(f"web_static packed: versions/web_static_{st}.tgz")
-        return f"versions/web_static_{st}.tgz"
-    except Exception as e:
-        return None
+env.key_filename = '~/.ssh/id_rsa'
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
     """ deploy tarball """
-    if os.path.exists(archive_path):
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
         archive = archive_path.split("/")[1]
         Uncompressed_file = archive_path.split("/")[1].split(".")[0]
         put(archive_path, "/tmp/")
         run(f"mkdir -p /data/web_static/releases/{Uncompressed_file}/")
-        run(f"tar -xzf /tmp/{archive} -C /data/web_static/releases/{Uncompressed_file}/")
+        run(f"tar -xzf /tmp/{archive} -C\
+            /data/web_static/releases/{Uncompressed_file}/")
         run(f"rm /tmp/{archive}")
-        run(f"mv -f /data/web_static/releases/{Uncompressed_file}/web_static/* /data/web_static/releases/{Uncompressed_file}/")
+        run(f"mv -f\
+            /data/web_static/releases/{Uncompressed_file}/web_static/*\
+            /data/web_static/releases/{Uncompressed_file}/")
         run(f"rm -rf /data/web_static/releases/{Uncompressed_file}/web_static")
         run(f"rm -rf /data/web_static/current")
-        run(f"ln -sf /data/web_static/releases/{Uncompressed_file}/ /data/web_static/current")
+        run(f"ln -sf /data/web_static/releases/{Uncompressed_file}/\
+            /data/web_static/current")
         print("New version deployed!")
         return True
-    else:
-        print("Error")
+    except Exception as e:
         return False
-    return False
