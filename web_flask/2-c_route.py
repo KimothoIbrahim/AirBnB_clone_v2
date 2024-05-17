60ef7a759301213cbee857311a6b9a0be3fbef51
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-05-17 09:00:27 +0300

    Make routes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,10 +10,12 @@ def greet():
     """ ftest function """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ to /hbnb """
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
