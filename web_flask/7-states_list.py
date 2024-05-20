895cdbf5059e2882c8454a3b89ccb25bd157d4a2
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-05-20 13:41:07 +0300

    talk to the database

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" setup to serve info from db """
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states_list")
+def states():
+    """ return all states """
+    States = storage.all("State").values()
+    return render_template("7-states_list.html", States=States)
+
+@app.teardown_appcontext
+def teardown(execption):
+    """ chnge session """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
