946ea7c47add401a0c0bc8699a308b27c171c175
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-05-21 11:58:33 +0300

    Dispay states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" setup to serve info from db """
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states():
+    """ return all states """
+    States = storage.all("State").values()
+    return render_template("7-states_list.html", States=States)
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities():
+    """ get cities """
+    States = storage.all("State").values()
+    return render_template("8-cities_by_states.html", States=States)
+
+@app.teardown_appcontext
+def teardown(execption):
+    """ chnge session """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
